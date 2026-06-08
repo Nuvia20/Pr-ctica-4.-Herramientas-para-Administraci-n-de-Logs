2d98b3ff8533fa68cc378514d02f1ab6138ffce3
Nuvia20 <2023371105@uteq.edu.mx>
2026-06-08 12:44:44 -0600

    Practicas P2A P2B P2C

---
+++ Views/Search/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VulnerableApp.Views.Search
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
